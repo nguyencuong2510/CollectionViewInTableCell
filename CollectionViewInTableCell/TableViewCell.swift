2c360886a65cbad5f9788c735812a20ca9ed1101
nguyencuong <nguyencuong@Admins-iMac.local>
2017-12-29 16:12:38 +0700

    end

---
+++ CollectionViewInTableCell/TableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  TableViewCell.swift
+//  CollectionViewInTableCell
+//
+//  Created by nguyencuong on 12/29/17.
+//  Copyright © 2017 nguyencuong. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var headerCollectionView: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        headerCollectionView.text = ""
+    }
+    
+    func setCollectionViewDataSourceDelegate
+        <D: UICollectionViewDataSource & UICollectionViewDelegate>
+        (dataSourceDelegate: D, forRow row: Int) {
+        
+        collectionView.delegate = dataSourceDelegate
+        collectionView.dataSource = dataSourceDelegate
+        collectionView.tag = row
+        collectionView.reloadData()
+    }
+
+}
+
+
---
+++ CollectionViewInTableCell/TableViewController.swift
@@ -0,0 +1,70 @@
+//
+//  TableViewController.swift
+//  CollectionViewInTableCell
+//
+//  Created by nguyencuong on 12/29/17.
+//  Copyright © 2017 nguyencuong. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var listArray: [[Int]] = [[0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9],[10,11,12,13,14,15,16,17,18,19,0,1,2,3,4,5,6,7,8,9],[20,21,22,23,24,25,26,27,28,29,0,1,2,3,4,5,6,7,8,9],[30,31,32,33,34,35,36,37,38,39,0,1,2,3,4,5,6,7,8,9]]
+    var listHeader: [String] = ["item 1", "item 2", "item 3", "item 4"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    // MARK: - Table view data source
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as! TableViewCell
+        
+        cell.headerCollectionView.text = listHeader[indexPath.row]
+        
+       return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        guard let tableViewCell = cell as? TableViewCell else { return }
+        tableViewCell.setCollectionViewDataSourceDelegate(dataSourceDelegate: self, forRow: indexPath.row)
+        
+    }
+
+}
+extension TableViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return listArray[collectionView.tag].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ReusableCell", for: indexPath) as! CollectionViewCell
+        
+        switch collectionView.tag {
+        case 0:
+            cell.imageIcon.image = UIImage(named: "default")
+        case 1:
+            cell.imageIcon.image = UIImage(named: "Image")
+        case 2:
+            cell.imageIcon.image = UIImage(named: "Image-1")
+        case 3:
+            cell.imageIcon.image = UIImage(named: "Image-2")
+        default:
+            cell.imageIcon.image = UIImage(named: "default")
+        }
+        
+        return cell
+    }
+    
+    
+    
+}
